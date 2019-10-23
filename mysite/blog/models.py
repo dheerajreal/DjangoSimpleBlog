b85bcd80a33ace1d0125f0133fb17d3bb24e6194
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-24 04:11:38 +0530

    added slug field

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+
 class BlogPost(models.Model):
     title = models.CharField(max_length=256)
-    content=models.TextField()
+    content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=256)
+
+    def slug(self):
+        return slugify(self.title)
 
     def __str__(self):
         return self.title
