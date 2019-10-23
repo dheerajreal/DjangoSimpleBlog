6951f0bdc5d62d882cbf48e9c60f9d3d9125a9e6
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-23 21:24:01 +0530

    added template for archive and about

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import BlogPost
 # Create your views here.
 
 
 def index(request):
 
-    blogs=BlogPost.objects.order_by('-date_created')
-    context={
-        "blogs":blogs,
-        }
-    return render(request,"blog/home.html",context)
+    blogs = BlogPost.objects.order_by('-date_created')
+    context = {
+        "blogs": blogs,
+    }
+    return render(request, "blog/home.html", context)
 
 
-def detail(request,number):
-    return HttpResponse(f"DETAIL {number}")
+def detail(request, number):
+    return render(request, "blog/detail.html")
 
 
 def archive(request):
-    return HttpResponse("ARCHIVE")
+    return render(request, "blog/archive.html")
+
 
 def contact(request):
-    return render(request,"blog/contact.html")
+    return render(request, "blog/contact.html")
 
 
 def about(request):
-    return render(request,"blog/about.html")
+    return render(request, "blog/about.html")
