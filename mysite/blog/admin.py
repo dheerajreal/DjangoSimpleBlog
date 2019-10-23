bc34ad1aa9ce5aa1995700f124f07e5cdd0de0da
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-23 22:34:40 +0530

    customized admin interface for BlogPost model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import BlogPost
 # Register your models here.
-admin.site.register(BlogPost)
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    # things to on admin page
+    list_display = ("title","date_created","date_updated")
+    # can filter by date_created
+    list_filter = ["date_created"]
+    # allow search
+    search_fields = ["title","content"]
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
