9fcc8483b96aa0909ac3cb2809ea5dc5e0dd27ac
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-23 20:38:50 +0530

    added some urls and views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,8 @@ from django.urls import path ,include
 from . import views
 urlpatterns = [
     path('', views.index,name="index"),
+    path('<int:number>/', views.detail,name="detail"),
+    path('archive/', views.archive,name="archive"),
+    path('contact/', views.contact,name="contact"),
+    path('about/', views.about,name="about"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("INDEX")
+
+
+def detail(request,number):
+    return HttpResponse(f"DETAIL {number}")
+
+
+def archive(request):
+    return HttpResponse("ARCHIVE")
+
+def contact(request):
+    return HttpResponse("CONTACT")
+
+
+def about(request):
+    return HttpResponse("ABOUT")
