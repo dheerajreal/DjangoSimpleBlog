df6e2620f2f9eb7b47a1561b8a4ef9eb2502b944
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-10-23 22:44:25 +0530

    customize admin interface

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 from .models import BlogPost
 # Register your models here.
 
+# change admin interface
+admin.site.index_title = "Blog admin"
+admin.site.site_header = "Blog admin"
+admin.site.site_title = "Blog admin"
+admin.site.site_url = "/blog"
 
 class BlogPostAdmin(admin.ModelAdmin):
     # things to on admin page
-    list_display = ("title","date_created","date_updated")
+    list_display = ("title", "date_created", "date_updated")
     # can filter by date_created
     list_filter = ["date_created"]
     # allow search
-    search_fields = ["title","content"]
+    search_fields = ["title", "content"]
 
 
 admin.site.register(BlogPost, BlogPostAdmin)
