b36344a23b3dc6ed9d6f1f028366adc7e87909c7
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-11-02 15:45:23 +0530

    added contact form to admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost, ContactFormEntry
 # Register your models here.
 
 # change admin interface
@@ -7,6 +7,7 @@ admin.site.index_title = "Blog admin"
 admin.site.site_header = "Blog admin"
 admin.site.site_title = "Blog admin"
 admin.site.site_url = "/"
+
 
 class BlogPostAdmin(admin.ModelAdmin):
     # things to on admin page
@@ -17,4 +18,13 @@ class BlogPostAdmin(admin.ModelAdmin):
     search_fields = ["title", "content"]
 
 
+class ContactFormEntryAdmin(admin.ModelAdmin):
+    # things to on admin page
+    list_display = ("name", "email", "message")
+    # can filter by date_created
+    list_filter = ["date_created"]
+    
+
+
 admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(ContactFormEntry,ContactFormEntryAdmin)
