bd9c95bf3218773a49f0cab7b05cc490afb672f1
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2019-12-31 02:02:53 +0530

    improved formatting

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index,name="index"),
-    path('<int:number>/', views.detail,name="detail"),
-    path('<int:number>/<slug:slug>/', views.detail,name="detail"),
-    path('archive/', views.archive,name="archive"),
-    path('contact/', views.contact,name="contact"),
-    path('about/', views.about,name="about"),
+    path('', views.index, name="index"),
+    path('<int:number>/', views.detail, name="detail"),
+    path('<int:number>/<slug:slug>/', views.detail, name="detail"),
+    path('archive/', views.archive, name="archive"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
 ]
