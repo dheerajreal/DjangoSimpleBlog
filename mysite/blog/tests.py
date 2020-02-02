56560fa7a79cf28ecb3ba3965c31b1437236a4c3
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2020-02-02 17:30:53 +0530

    added some tests

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ResponseCodeTest(TestCase):
+    def test_home_page_response_code(self):
+        response = self.client.get("/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_about_page_response_code(self):
+        response = self.client.get("/about/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_contact_page_response_code(self):
+        response = self.client.get("/contact/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_archive_page_response_code(self):
+        response = self.client.get("/archive/")
+        self.assertEquals(response.status_code, 200)
